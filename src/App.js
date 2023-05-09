711a50f90cee1d6c7830fdc216c9a829607c788d
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-09 13:50:07 -0500

    Update App.js
    following aws docs...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const App = () => {
   return (
@@ -8,10 +9,10 @@ const App = () => {
       <header className="App-header">
         <Authenticator>
           {({ signOut, user }) => (
-            <>
+            <main>
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Greetings!</h1>
-            </>
+            </main>
           )}
         </Authenticator>
       </header>
